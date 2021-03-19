eb317617cb72ba87360dea8c7cbd19baf75e87e1
npgoalkeper <npgoalkeper@gmail.com>
2021-03-19 01:45:07 -0700

    play with dependencies a bit, no actual rewrites just yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SI1145.h>
+#include <Adafruit_ILI9341.h>
+#include <XPT2046_Touchscreen.h>
+#include <DHT.h>
 
 void setup() {}
 
